6cc5ab8c3f8bb74d8a13b5e3a5310ae6de8b9709
Hong Yan <homer.yan@gmail.com>
2016-12-11 23:13:56 -0700

    Reuse DHT

--- test/index.js
+++ test/index.js
@@ -1,22 +1,36 @@
+const DHT = require('bittorrent-dht');
 const fetchMetadata = require('../index');
+const defaults = require('../lib/defaults');
 
 // infohash of ubuntu-16.04.1-server-amd64.iso
 const INFO_HASH = '90289fd34dfc1cf8f316a268add8354c85334458';
+const INFO_HASH2 = 'e4fa3ccbbf7dcb3c620af6c59cfb4936ff0e3616';
 
-fetchMetadata(INFO_HASH, { maxConns: 10, fetchTimeout: 30000, socketTimeout: 5000 },
-  (err, metadata) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(`[Callback] ${metadata.info.name.toString('utf-8')}`);
-  });
+// const dht = new DHT({concurrency: defaults.DEFAULT_KRPC_CONCURRENCY});
+const dht = new DHT({concurrency: 32});
 
-fetchMetadata(INFO_HASH, { maxConns: 10, fetchTimeout: 30000, socketTimeout: 5000 })
+//
+// fetchMetadata(INFO_HASH, { maxConns: 100, fetchTimeout: 30000, socketTimeout: 5000 },
+//   (err, metadata) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log(`[Callback] ${metadata.info.name.toString('utf-8')}`);
+//   });
+
+fetchMetadata(INFO_HASH, { maxConns: 100, fetchTimeout: 30000, socketTimeout: 5000, dht })
   .then(metadata => {
     console.log(`[Promise] ${metadata.info.name.toString('utf-8')}`);
   }).catch(err => {
     console.log(err);
+});
+
+fetchMetadata(INFO_HASH2, { maxConns: 100, fetchTimeout: 30000, socketTimeout: 5000, dht })
+  .then(metadata => {
+    console.log(`[Promise] ${metadata.info.name.toString('utf-8')}`);
+  }).catch(err => {
+  console.log(err);
 });
 
 // fetchMetadata.fromPeer(INFO_HASH, 'IP_ADDRESS:PORT', {timeout: 5000},
