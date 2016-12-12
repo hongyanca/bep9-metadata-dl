ea274da31ebd4b2ee548f57671b396f49b2cbe70
Hong Yan <homer.yan@gmail.com>
2016-12-11 23:26:31 -0700

    Reuse DHT

--- test/index.js
+++ test/index.js
@@ -1,15 +1,13 @@
 const DHT = require('bittorrent-dht');
 const fetchMetadata = require('../index');
-const defaults = require('../lib/defaults');
 
 // infohash of ubuntu-16.04.1-server-amd64.iso
 const INFO_HASH = '90289fd34dfc1cf8f316a268add8354c85334458';
-const INFO_HASH2 = 'e4fa3ccbbf7dcb3c620af6c59cfb4936ff0e3616';
+// infohash of ubuntu-16.04.1-desktop-amd64.iso
+const INFO_HASH2 = '9f9165d9a281a9b8e782cd5176bbcc8256fd1871';
 
-// const dht = new DHT({concurrency: defaults.DEFAULT_KRPC_CONCURRENCY});
-const dht = new DHT({concurrency: 32});
+const dht = new DHT({ concurrency: 32 });
 
-//
 // fetchMetadata(INFO_HASH, { maxConns: 100, fetchTimeout: 30000, socketTimeout: 5000 },
 //   (err, metadata) => {
 //     if (err) {
